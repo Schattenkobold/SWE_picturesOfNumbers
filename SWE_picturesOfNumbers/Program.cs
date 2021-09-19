cf1e1a3d8fbd28aacb4d1522b9bd56a8eb7f2377
Schattenkobold <consti.akamphuber@gmail.com>
2021-09-19 15:41:08 +0200

    The actual files while go to the wide world web. Let's Go on an adventure! -my model, probably, not bilbo, i'm 97% sure

---
+++ SWE_picturesOfNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWE_picturesOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
